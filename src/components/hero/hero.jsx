22279534b0b47f04c716abe15021ae8e5084ce11
marzieh <marziehmonshizade@yahoo.com>
2023-08-23 16:37:27 +0330

    refactor routes

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,57 +1,64 @@
-import React,{useState} from 'react'
-import { logout ,useAuth } from "../fire/firebase";
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { logout, useAuth } from "../fire/firebase";
 
-import Boxs from '../boxs/boxStructure/boxs';
+import Boxs from "../boxs/boxStructure/boxs";
 
 const Hero = () => {
-    const currentUser = useAuth();
-    const [ loading, setLoading ] = useState(false);
-    let history = useNavigate();
+  const currentUser = useAuth();
+  const [loading, setLoading] = useState(false);
+  let history = useNavigate();
 
-    async function handleLogout() {
-        setLoading(true);
-        try {
-          await logout();
-          history('/');
-        } catch {
-          alert("Error!");
-        }
-        setLoading(false);
-      }
-    return ( 
-        <>
-        <Boxs bg="bg-success">
-            <div className="border-bottom p-2 fs-3">Welcome to Wikipedia :<span style={{color:"#1e713f"}}>{currentUser?.email}</span></div>
-            <div className="container mt-3">
-              <div className="row">
-                <div className="col-12 col-md-6 text-center">
-                   <p className='text-muted'>To enter the main page, please click the button below</p> 
-                  <button type="submit" 
-                          className="btn w-25 bg-dark" 
-                          disabled={ loading || !currentUser } 
-                          onClick={() =>history('/')} 
-                          >
-                            Main page
-                  </button>
-                </div>
-                <div className="col-12 col-md-6 text-center">
-                   <p className='text-muted'>To exit, please click the button below</p>
-                    <button type="submit" 
-                            className="btn w-25 bg-dark" 
-                            disabled={ loading || !currentUser } 
-                            onClick={handleLogout} 
-                            >
-                              Log out
-                    </button>
-                </div>
-              </div>
+  async function handleLogout() {
+    setLoading(true);
+    try {
+      await logout();
+      history("/");
+    } catch {
+      alert("Error!");
+    }
+    setLoading(false);
+  }
+  return (
+    <>
+      <Boxs bg="bg-success">
+        <div className="border-bottom p-2 fs-3">
+          Welcome to Wikipedia :
+          <span style={{ color: "#1e713f" }}>{currentUser?.email}</span>
+        </div>
+        <div className="container mt-3">
+          <div className="row">
+            <div className="col-12 col-md-6 text-center">
+              <p className="text-muted">
+                To enter the main page, please click the button below
+              </p>
+              <button
+                type="submit"
+                className="btn w-25 bg-dark"
+                disabled={loading || !currentUser}
+                onClick={() => history("/wikipedia")}
+              >
+                Main page
+              </button>
             </div>
-           
-           
-        </Boxs>
-        </>
-     );
-}
- 
+            <div className="col-12 col-md-6 text-center">
+              <p className="text-muted">
+                To exit, please click the button below
+              </p>
+              <button
+                type="submit"
+                className="btn w-25 bg-dark"
+                disabled={loading || !currentUser}
+                onClick={handleLogout}
+              >
+                Log out
+              </button>
+            </div>
+          </div>
+        </div>
+      </Boxs>
+    </>
+  );
+};
+
 export default Hero;
--- src/components/logIn/logIn.jsx
+++ src/components/logIn/logIn.jsx
@@ -1,90 +1,112 @@
-import React, {useState, useRef} from 'react'
-import { login ,useAuth } from "../fire/firebase";
-import { useNavigate } from 'react-router-dom';
+import React, { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { login, useAuth } from "../fire/firebase";
 
-import Boxs from '../boxs/boxStructure/boxs';
-
+import Boxs from "../boxs/boxStructure/boxs";
 
 const Login = () => {
-    const emailRef = useRef();
-    const [emailError, setEmailError] =useState()
-    const passwordRef = useRef();
-    const [passwordError, setPasswordError] =useState()
-    const [ loading, setLoading ] = useState(false);
-    const currentUser = useAuth();
+  const emailRef = useRef();
+  const [emailError, setEmailError] = useState();
+  const passwordRef = useRef();
+  const [passwordError, setPasswordError] = useState();
+  const [loading, setLoading] = useState(false);
+  const currentUser = useAuth();
 
-    let history = useNavigate();
-     
-   const clearError = () =>{
-    setEmailError('');
-    setPasswordError('')
-   } 
-    async function handleLogin() {
-        setLoading(true);
-        clearError();
-        try {
-          await login(emailRef.current.value, passwordRef.current.value);
-          history('/hero');
-        }catch(error) {
-            switch(error.code){
-                case 'auth/invalid-email':
-                case 'auth/user-disabled':
-                case 'auth/user-not-found':
-                    setEmailError(error.message);
-                    break;
-                case 'auth/wrong-password':
-                    setPasswordError(error.code);
-                    break;
-                default:
-                    break;
-            }
-        }
-        setLoading(false);
+  let history = useNavigate();
+
+  const clearError = () => {
+    setEmailError("");
+    setPasswordError("");
+  };
+  async function handleLogin() {
+    setLoading(true);
+    clearError();
+    try {
+      await login(emailRef.current.value, passwordRef.current.value);
+      history("/hero");
+    } catch (error) {
+      switch (error.code) {
+        case "auth/invalid-email":
+        case "auth/user-disabled":
+        case "auth/user-not-found":
+          setEmailError(error.message);
+          break;
+        case "auth/wrong-password":
+          setPasswordError(error.code);
+          break;
+        default:
+          break;
       }
-    return ( 
-        <Boxs>
-         <form> 
-           <h1 className='border-bottom'>Log in</h1>
-           <div className="d-flex flex-column align-items-center mt-5">
-            <label className="mb-3 w-50">
-                    <span htmlFor="InputEmail" className="form-label">Email</span>
-                    <input type="email" required className="form-control" id="InputEmail" 
-                    ref={emailRef} 
-                    placeholder='Enter your Email'/>
-            </label>
-            <p style={{color:"red"}}>{emailError}</p>
-            <label className="mb-3 w-50 ">
-                <span htmlFor="exampleInputPassword1" className="form-label">Password</span>
-                <input 
-                    type="password" 
-                    className="form-control" 
-                    id="exampleInputPassword1" 
-                    placeholder='Enter your password' 
-                    ref={passwordRef} 
-                    />
-            </label>
-            <p style={{color:"red"}}>{passwordError}</p>
-            <button type="submit" 
-                            className="btn btn-primary col-6 mx-auto" 
-                            disabled={ loading || currentUser } 
-                            onClick={handleLogin}>log in</button>
-            <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="#EAECF0" className="bi bi-people-fill" viewBox="0 0 16 16">
-                <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                <path fillRule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/>
-                <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
-            </svg>
-        
-                    <button type="submit" 
-                            className="btn btn-secondary  col-6 mx-auto" 
-                            onClick={() => history('/createaccount')}>
-                            join wikipedia
-                   </button>
-           </div>
-    
-         </form>
-        </Boxs>
-     );
-}
+    }
+    setLoading(false);
+  }
+  return (
+    <Boxs>
+      <form>
+        <h1 className="border-bottom">Log in</h1>
+        <div className="d-flex flex-column align-items-center mt-5">
+          <label className="mb-3 w-50">
+            <span htmlFor="InputEmail" className="form-label">
+              Email
+            </span>
+            <input
+              type="email"
+              required
+              className="form-control"
+              id="InputEmail"
+              ref={emailRef}
+              placeholder="Enter your Email"
+            />
+          </label>
+          <p style={{ color: "red" }}>{emailError}</p>
+          <label className="mb-3 w-50 ">
+            <span htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </span>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter your password"
+              ref={passwordRef}
+            />
+          </label>
+          <p style={{ color: "red" }}>{passwordError}</p>
+          <button
+            type="submit"
+            className="btn btn-primary col-6 mx-auto"
+            disabled={loading || currentUser}
+            onClick={handleLogin}
+          >
+            log in
+          </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="200"
+            height="200"
+            fill="#EAECF0"
+            className="bi bi-people-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+            <path
+              fillRule="evenodd"
+              d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"
+            />
+            <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
+          </svg>
 
- 
+          <button
+            type="submit"
+            className="btn btn-secondary  col-6 mx-auto"
+            onClick={() => history("/createaccount")}
+          >
+            join wikipedia
+          </button>
+        </div>
+      </form>
+    </Boxs>
+  );
+};
+
 export default Login;
